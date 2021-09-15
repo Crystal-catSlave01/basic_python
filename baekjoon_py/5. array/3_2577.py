7a65fe4a62d3b8961425b0047ff5eeecb00ebbf2
Crystal_Cat <tmddn0505@gmail.com>
2021-09-15 22:57:17 +0900

    hmm...

--- baekjoon_py/5. array/3_2577.py
+++ baekjoon_py/5. array/3_2577.py
@@ -2,17 +2,7 @@ a = int(input())
 b = int(input())
 c = int(input())
 
-r = str(a*b*c)
-l = []
+r = list(str(a * b * c))
 
-
-for i in range(0,len(r)):
-    cnt = 0
-
-    for j in range(0,10):
-        if r[i] == j:
-            cnt += 1
-            l[j] = cnt
-    print(l[j])  
-
-print(r)
+for i in range(10):
+    print(r.count(str(i)))
