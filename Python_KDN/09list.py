486e09f1d4856993e2eb12664e9f2dd986e78501
SoftWarePark <tmddn0505@gmail.com>
2022-01-13 13:48:30 +0900

    한전 KDN 파이썬 리스트 추가

---
+++ Python_KDN/09list.py
@@ -0,0 +1,22 @@
+#List
+
+#mutable
+val1 = [1,2,3]
+val1[0] = 'p'
+print(val1)
+
+#emutable
+val = 'hello'
+# val[0] = 'p'
+print(val)
+
+#remove
+value = [1, 2, 3, 2]
+value.remove(3)
+print(value)
+
+#pop
+value = [1, 2, 3, 4, 5]
+result = value.pop()
+print(result)
+print(value)
