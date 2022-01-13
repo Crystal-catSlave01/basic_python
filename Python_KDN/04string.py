08d02065c14150e2e593c64de1cf801f7e234baf
SoftWarePark <tmddn0505@gmail.com>
2022-01-13 11:14:48 +0900

    한전 KDN 파이썬 문자열 len, 인덱싱, 슬라이싱

--- Python_KDN/04string.py
+++ Python_KDN/04string.py
@@ -1,5 +1,18 @@
 #문자열 타입 (+랑 * 연산만 가능)
 
-print('')
+myString = ['문자열', 'python', '\'', 's', 'easy!']
+print(myString)
+print("""문자열 python's easy!""")
+
 # ''문자열 python's easy!
 
+data = 'Python'
+
+print(len(data))
+
+print(data[len(data)-1])
+
+print(data[-1])
+
+# 시작점 : 끝점 : 증감정도 (끝점은 포함 X)
+print(data[0:3:2])
