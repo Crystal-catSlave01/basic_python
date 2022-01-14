51ff63cc58515e85fff976090fad9b2f7c114678
SoftWarePark <tmddn0505@gmail.com>
2022-01-14 10:12:07 +0900

    한전 KDN 파이썬 dictionary data.get

--- Python_KDN/12dictionary.py
+++ Python_KDN/12dictionary.py
@@ -16,7 +16,7 @@ del data['1']
 # 추가 = 할당
 # 삭제 del
 
-data = {'1' : 'one', 2 : 'two', (1,2) : '345'}
+data = {'1' : 'one', '2' : 'two', (1,2) : '345'}
 result = data.keys() # keys() .key값 찾기, .values() value 찾기, .items() = 튜플 형태 -> key : value
 print(result)
 print(type(result))
@@ -24,8 +24,19 @@ print(list(result))
 # keys()
 print(data)
 
+# asdasd
 
 for key, value in data.items():
     print(f'key:{key} / value: {value}')
 
 # list와 dict에서 값을 찾는 것 ->  O(N) : O(1~N) = list : dict
+
+print(data.get('3'))
+
+check = ['1', '2', '3']
+
+for num in check:
+    if data.get(num):
+        print(data[num])
+    else:
+        print('키값 없음')
