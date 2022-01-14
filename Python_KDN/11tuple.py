10508f260b5b0df1d11e6face48f01b15495e87a
SoftWarePark <tmddn0505@gmail.com>
2022-01-14 09:48:53 +0900

    한전 KDN 파이썬 dictionary 추가

--- Python_KDN/11tuple.py
+++ Python_KDN/11tuple.py
@@ -7,3 +7,14 @@ packing
 Unpacking
 패킹된 변수에서 여러 개의 값을 꺼내는 것
 """
+
+a = 1, 3, 5
+x, y, z =a
+print(a)
+print(type(a))
+
+print(f'x: {x}, y: {y}, z: {z}')
+
+# data = (1,2,3)
+# data[2] = 5
+# print(data) 에러가 남 ( 튜플은 mutable)
--- Python_KDN/12dictionary.py
+++ Python_KDN/12dictionary.py
@@ -6,8 +6,20 @@
 data ={
     '1' : 'one',
     '2' : 'two',
-    1 : 1,
-    2 : 2
 }
 
-print(data['3'])
+data['3'] = 'three'
+del data['1']
+
+# print(data['3'])
+
+# 추가 = 할당
+# 삭제 del
+
+data = {'1' : 'one', 2 : 'two', (1,2) : '345'}
+result = data.keys() # keys() .key값 찾기, .values() value 찾기, .items() = 튜플 형태 -> key : value
+print(result)
+print(type(result))
+print(list(result))
+# keys()
+print(data)
