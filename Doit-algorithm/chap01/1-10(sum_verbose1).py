3a032b77d28a4df8dffe96a50658f9049e0b1c9e
zip-sa <tmddn0505@gmail.com>
2022-01-19 23:15:49 +0900

    1-10 추가

---
+++ Doit-algorithm/chap01/1-10(sum_verbose1).py
@@ -0,0 +1,18 @@
+# a부터 b까지 정수의 합 구하기1
+
+print('a부터 b까지 정수의 합을 구합니다.')
+a = int(input('a: '))
+b = int(input('b: '))
+
+if a > b:
+    a, b = b, a
+
+sum = 0
+for i in range(a,b+1):
+    if i < b:
+        print(f'{i} + ', end='')
+    else:
+        print(f'{i} = ', end='')
+    sum += i
+
+print(sum)
