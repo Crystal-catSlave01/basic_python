03d28747cea9e40bd19d751bc49081cf7ca4b51a
SoftWarePark <tmddn0505@gmail.com>
2022-01-22 23:40:50 +0900

    1-14 코드 작성

--- Doit-algorithm/chap01/1-14(print_stars1).py
+++ Doit-algorithm/chap01/1-14(print_stars1).py
@@ -1,1 +1,13 @@
 # *를 n개 출력하되 w개마다 줄바꿈하기 1
+
+print('It will be printed star')
+n = int(input('How many stars do you want to print? : '))
+w = int(input('How many stars should I change the line? : '))
+
+for i in range(n):
+    print('*', end='')
+    if i % w == w - 1:
+        print()
+
+if n % w:
+    print()
